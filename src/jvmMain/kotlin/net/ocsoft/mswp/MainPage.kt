9b5c393c25704a2dfc450c25b1331be84476c88b
Toshiyuki Ogawa <ogawa@oh-laser.com>
2019-07-26 20:10:44 +0900

    I had resource files in web program directry.

--- src/jvmMain/kotlin/net/ocsoft/mswp/MainPage.kt
+++ src/jvmMain/kotlin/net/ocsoft/mswp/MainPage.kt
@@ -1,22 +1,14 @@
 package net.ocsoft.mswp
 
-import javax.servlet.http.HttpServlet
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
-import javax.servlet.annotation.WebServlet
-
 /**
  * main page display
  */
-@WebServlet(name = "Home", value = ["/hello"])
-actual class MainPage : HttpServlet() {
-
-    override fun doGet(req: HttpServletRequest, res: HttpServletResponse) {
-        res.writer.write("This is java server page.")
-    }
-
+actual class MainPage {
 
    actual fun setupBody(model : Model, camera: Camera, 
         pointLight: PointLight) {
    }
 }
+
+
+
