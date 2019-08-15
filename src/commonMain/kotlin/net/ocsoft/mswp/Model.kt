8195bdd929114f083e89756c09a787ec73eebec0
Toshiyuki Ogawa <ogawa@oh-laser.com>
2019-08-16 05:33:48 +0900

    I removed some lines not to be used in program.

--- src/commonMain/kotlin/net/ocsoft/mswp/Model.kt
+++ src/commonMain/kotlin/net/ocsoft/mswp/Model.kt
@@ -5,7 +5,7 @@ package net.ocsoft.mswp
 class Model(rowCount: Int = 20, 
     columnCount: Int = 20,
     val physicsEng: PhysicsEng = PhysicsEng(),
-    val logic: Logic = Logic(rowCount, columnCount, 5)) {
+    val logic: Logic = Logic(rowCount, columnCount, 3)) {
     init {
         logic.status = Status()
     }	
