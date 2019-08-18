3a825b1a16e665d761a3e320bc373a102aefa9f7
Toshiyuki Ogawa <toshiyuki.ogawa.oc@gmail.com>
2019-08-18 14:40:53 +0900

    I added the link to the project source code.

--- about.php
+++ about.php
@@ -3,11 +3,16 @@
   <meta charset="UTF-8">
   <title>About this projects</title>
   <meta name="viewport" content="width=device-width initial-scale=1">
-
+<!--
   <script
     src="https://code.jquery.com/jquery-3.4.1.min.js"
     integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
     crossorigin="anonymous"></script>
+-->
+  <script
+    src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+  </script>
+
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -50,6 +55,8 @@
 <p>静的型志向言語は、言語習得レベルが異なる開発者が多数集まるような大規模開発に向いているといえる。開発後半になって、プログラムが具体的になるほど、子供たちから拡張の提案でてくるようになってきた。</p>
 <h1>感想</h1>
 <p>タイムを計ったり、ゲームの難易度を設定したり、子供たちからのアイディアを反映させていきたい。</p>
+<h1>資料など</h1>
+<a href="https://rs-app.oc-soft.net/git/?p=rs-app/mswp.git;a=tree" class="stretched-link">ソースコード</a>
 </div>
 </body>
 </html>
