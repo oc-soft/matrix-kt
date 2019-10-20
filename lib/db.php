58015c927b85b6a6a17cd592cd10fd10c920ac14
Toshiyuki Ogawa <ogawa@oh-laser.com>
2019-10-21 06:43:32 +0900

    You can specify port and socket for mysql connection.

--- lib/db.php
+++ lib/db.php
@@ -22,9 +22,20 @@ class DB {
     function get_sql_client() {
         if (!isset($this->sql_client)) {
             global $db_settings;
+            if (isset($db_settings['port'])) {
+                $port = $db_settings['port'];
+            } else {
+                $port = ini_get('mysqli.default_port');
+            }
+            if (isset($db_settings['socket'])) {
+                $socket = $db_settings['socket'];
+            } else {
+                $socket = ini_get('mysqli.default_socket');
+            }
             $this->sql_client = new mysqli($db_settings['host'],
                 $db_settings['user'], $db_settings['password'],
-                $db_settings['db']);
+                $db_settings['db'],
+                $port, $socket);
         }
         return $this->sql_client;
     }
