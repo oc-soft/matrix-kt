f1a4e4b729bbcb183c4f83638e76e337ee674345
Toshiyuki Ogawa <toshi@oc-soft.net>
2020-10-29 06:27:45 +0900

    I moved all js libraries to webpack configuration entries.

--- src/jsMain/js/libraries.js
+++ src/jsMain/js/libraries.js
@@ -1,6 +1,3 @@
-import '@fortawesome/fontawesome-free'
-// import '@popperjs/core'
-import 'bootstrap'
-import 'webfontloader'
-// import 'less';
+//import '@fortawesome/fontawesome-free'
+// import 'bootstrap'
 // vi: se ts=2 sw=2 et:
