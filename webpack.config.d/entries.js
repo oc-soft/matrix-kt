d7aeb8d2e9e7575afb05e810b21464502c6c3a4d
Toshiyuki Ogawa <toshi@oc-soft.net>
2020-10-29 06:29:41 +0900

    I moved all js libraries to webpack configuration entries.

--- webpack.config.d/entries.js
+++ webpack.config.d/entries.js
@@ -17,10 +17,49 @@ class Entries {
   setupWebpack(config) {
     let libMod = './libraries.js';
     config.entry = config.entry || { };
-    if (libMod) {
-      config.entry.libs = libMod;
+
+    config.entry['fontawesome-free'] = '@fortawesome/fontawesome-free'
+    config.entry.bootstrap = 'bootstrap'
+    config.entry.kotlin = 'kotlin'
+    // to run this, you have to run webpack which has version haigher than 5.0
+    // this.setupMainEntry(config)
+  }
+  /**
+   * setup main etry with dedepency 
+   */
+  setupMainEntry(config) {
+    if (config.entry.main) {
+      let mainEntry
+      if (Array.isArray(config.entry.main)) {
+        mainEntry = {
+          import: config.entry.main
+        }
+        config.entry.main = mainEntry
+      } else if (typeof config.entry.main === 'string') {
+        importEntry = config.entry.main
+        mainEntry = {
+          import: config.entry.main
+        }
+        config.entry.main = mainEntry
+      } else if (typeof config.entry.main === 'object'){
+        mainEntry = config.entry.main 
+      }
+      let dependOn
+      if (mainEntry) {
+        if (Array.isArray(mainEntry.dependOn)) {
+          dependOn = mainEntry.dependOn
+        } else if (typeof mainEntry.dependOn === 'string') {
+          dependOn = [mainEntry.dependOn]
+          mainEntry.dependOn = dependOn
+        } else {
+          dependOn = []
+          mainEntry.dependOn = dependOn
+        }
+      }
+      if (dependOn) {
+        dependOn.unshift('fontawesome-free', 'bootstrap', 'kotlin')
+      }
     }
-    config.entry.kotlin = ['kotlin'];
   }
 }
 
