585b986daab3684d5c46ae1cdcda0b7eb2548398
Toshiyuki Ogawa <toshi@oc-soft.net>
2020-11-08 04:59:39 +0900

    I work in progress to create core libraries for oc-soft.

--- entry-less.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once implode('/', array(__DIR__, 'lib', 'color-scheme-response.php'));
-
-require 'template/entry-less.php';
-// vi: se ts=4 sw=4 et:
--- template/config-less.php
+++
@@ -1,5 +0,0 @@
-@env-color-background: <?php echo $env_colors[0]->toHexStr(); ?>;
-@env-color-foreground: <?php echo $env_colors[1]->toHexStr(); ?>;
-@text-color: @env-color-foreground;
-<?php
-/* vi: se ts=4 sw=4 et: */
